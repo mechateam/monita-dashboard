6c0582c2727ef533d65209d2f6d5256a317bfc89
Meldi <meldi.sayoko@pinhome.id>
2021-09-13 22:10:44 +0700

    Initial Commit

---
+++ src/main/java/ta/simonita/monita/MonitaApplication.java
@@ -0,0 +1,13 @@
+package ta.simonita.monita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ta/simonita/monita/MonitaApplicationTests.java
@@ -0,0 +1,13 @@
+package ta.simonita.monita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
